7e65418d9a61776bbcab58748f0076b5644aff87
Miha <mihapr@siol.net>
2024-04-19 23:31:08 +0200

    ...

---
+++ myReactApp/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,13 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "phonegap-nfc.NFC",
+      "file": "plugins/phonegap-nfc/www/phonegap-nfc.js",
+      "pluginId": "phonegap-nfc",
+      "runs": true
+    }
+  ];
+  module.exports.metadata = {
+    "phonegap-nfc": "1.2.0"
+  };
+});
